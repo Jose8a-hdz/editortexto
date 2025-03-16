18b18fb52f27a9419da19911c04266dfafe438d2
Jose8a-hdz <al23760815@ite.edu.mx>
2025-03-15 18:47:20 -0700

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,8 @@
-print ('hola')
+import tkinter as tk
+from tkinter import messagebox
+#crear ventana
+root = tk.Tk()
+root.withdraw()
+def mostrar_alerta():
+    messagebox.showinfo('noti','hola')
+mostrar_alerta()
