34b2a4b769062a3e8c60063fc319606fe11d029b
Jose8a-hdz <al23760815@ite.edu.mx>
2025-03-15 19:36:10 -0700

    Update index.py

--- index.py
+++ index.py
@@ -1,8 +1,16 @@
-import tkinter as tk
-from tkinter import messagebox
-#crear ventana
-root = tk.Tk()
-root.withdraw()
-def mostrar_alerta():
-    messagebox.showinfo('noti','hola')
-mostrar_alerta()
+from tkinter import *
+
+ventana = Tk()
+menu = Menu(ventana)
+ventana.config(menu=menu)
+
+File=Menu(menu, tearoff=0)
+File.add_command(label='new proyect')
+Edit=Menu(menu, tearoff=0)
+Help=Menu(menu, tearoff=0)
+menu.add_cascade(label="File", menu =File)
+menu.add_cascade(label="Edit", menu =Edit)
+menu.add_cascade(label="Help", menu =Help)
+
+ventana.title('hola')
+ventana.mainloop()
